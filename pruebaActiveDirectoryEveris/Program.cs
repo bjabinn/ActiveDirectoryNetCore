d78e8d4fe9a1460ca957be8214e2653eb177e03d
Jesus Muñoz Galiano <jmunozga@everis.com>
2018-11-08 12:16:27 +0100

    Se documenta el código

--- pruebaActiveDirectoryEveris/Program.cs
+++ pruebaActiveDirectoryEveris/Program.cs
@@ -13,19 +13,51 @@ namespace pruebaActiveDirectory2
 {
     class Program
     {
-        
+        // Este método sirve para hacer login contra el AD de Everis.
+        // Es importante tener en cuenta que el párametro loginDn
+        // Debe tener el siguiente formato:
+        // loginDn="Usersad\\jmunozga"
+
+        // Devuelve un booleano con el resultado del éxito de la operación
         public bool checkUser(String loginDn,String password){
             bool res=false;
             DirectoryEntry entry = new DirectoryEntry();
             entry.Username=loginDn;
             entry.Password=password;
             DirectorySearcher searcher = new DirectorySearcher(entry);
+            // El campo userAccountControl guarda una serie de bits de control:
+             // Script = 1,                                     // 0x1
+            // AccountDisabled = 2,                            // 0x2
+            // HomeDirectoryRequired = 8,                      // 0x8
+            // AccountLockedOut = 16,                          // 0x10
+            // PasswordNotRequired = 32,                       // 0x20
+            // PasswordCannotChange = 64,                      // 0x40
+            // EncryptedTextPasswordAllowed = 128,             // 0x80
+            // TempDuplicateAccount = 256,                     // 0x100
+            // NormalAccount = 512,                            // 0x200
+            // InterDomainTrustAccount = 2048,                 // 0x800
+            // WorkstationTrustAccount = 4096,                 // 0x1000
+            // ServerTrustAccount = 8192,                      // 0x2000
+            // PasswordDoesNotExpire = 65536,                  // 0x10000 (Also 66048 )
+            // MnsLogonAccount = 131072,                       // 0x20000
+            // SmartCardRequired = 262144,                     // 0x40000
+            // TrustedForDelegation = 524288,                  // 0x80000
+            // AccountNotDelegated = 1048576,                  // 0x100000
+            // UseDesKeyOnly = 2097152,                        // 0x200000
+            // DontRequirePreauth = 4194304,                   // 0x400000
+            // PasswordExpired = 8388608,                      // 0x800000 (Applicable only in Window 2000 and Window Server 2003)
+            // TrustedToAuthenticateForDelegation = 16777216,  // 0x1000000
+            // NoAuthDataRequired = 33554432                   // 0x2000000
+
+            // El 2 nos indica si el usuario esta de baja y por tanto no 
+            // está en la empresa.
             searcher.Filter = "(&(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))";
             try{
                 searcher.FindOne();
                 res=true;
             }catch (COMException) {
-                /* 
+                /* Podemos saber si hay error de password de la siguiente manera.
+
                 // Autentication Error.
                 if (ex.ErrorCode == -2147023570) {
                     res=false;
@@ -33,20 +65,41 @@ namespace pruebaActiveDirectory2
             }
             return res;
         }
+        // Esta es la versión más eficiente para buscar empleados de Everis por 
+        // nombre corto (= SamAccountName en AD).
+        // ó nombre completo (= DisplayName en AD).
+
+        // Por eficiencia se propone mutilar a n resultados. 
+        // Es el parametro pageSize por defecto a 5.
+
+        // Devuelve en un directorio la fotos que cumplen el patrón 
+        // con el nombre corto.jpg
         public void getDirectorySearcher(String stringSearch, int pageSize=5){
             DirectorySearcher searcher = new DirectorySearcher();
+            // Esta es la manera de mutilar el número de resultados.
             searcher.SizeLimit=pageSize;
+            // Desactivamos el timeout. En principio no sería necesario.
             searcher.ClientTimeout=TimeSpan.FromSeconds(-1);
-            searcher.Filter = string.Format("(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(|(DisplayName=*{0}*)(SamAccountName=*{0}*)))",stringSearch,true);
+            // A continuación el patrón.
+            // Con userAccountControl controlamos que el usuario esté enable (ver método anterior)
+            // Se permite * para subcadenas.
+            searcher.Filter = string.Format("(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(|(DisplayName=*{0}*)(SamAccountName=*{0}*)))",stringSearch);
+            // Por eficiencia indicamos los parámetros a recuperar. 
+            // Esto mejora el tiempo
             searcher.PropertiesToLoad.Add("SamAccountName");
             searcher.PropertiesToLoad.Add("thumbnailPhoto");
             using (SearchResultCollection results = searcher.FindAll()) {
                 foreach (SearchResult domainUsers in results) {
                     DirectoryEntry user = domainUsers.GetDirectoryEntry();
                     if(user!=null){
+                        // Cacheamos por el nombre corto que puede constituir 
+                        // una clave primeria. Con esto conseguimos más eficiencia.
                         user.RefreshCache(new String[]{"SamAccountName"});
+                        // Extraemos el nombre corto y la foto.
                         String samAccountName= user.Properties["SamAccountName"][0].ToString();
                         var photoP=user.Properties["thumbnailPhoto"].Value;
+                        // Como ejemplo del método las salvamos en un directorio.
+                        // quizás se deberían devolver de forma rest.
                         if(photoP!=null){
                             byte[] photoInBytes=(byte[])photoP;
                             Bitmap photo = new Bitmap(new MemoryStream(photoInBytes)); 
@@ -56,6 +109,10 @@ namespace pruebaActiveDirectory2
                 }
             }
         }
+        // Implementación alternativa a la busqueda de empleados de Everis.
+        // Su implementación es más simple puesto que utiliza un objeto de 
+        // más alto nivel pero es menos eficiente. 
+        // alrededor de 10 veces en media.
         public void getUsersPrincipal(String stringSearch, int pageSize=5)
         {
             using (var ctx= new PrincipalContext(ContextType.Domain)){
@@ -83,6 +140,11 @@ namespace pruebaActiveDirectory2
         }
         static void Main(string[] args)
         {
+            // Un simple main para mostrar las llamadas 
+            // a los metodos anteriores.
+            // se prueba el tiempo de las versiones de búsqueda de 
+            // empleados.
+            
             Program p = new Program();
            //p.checkUser("Usersad\\jmunozga","Temporal14");
             
